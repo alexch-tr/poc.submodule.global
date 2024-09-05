0eef3f3611e745c8a1fe0eebde4e19d90ad96f2e
alexch <alexch@etoro.com>
2024-09-05 17:54:29 +0300

    calc

---
+++ POC.SubModule.Global/Program.cs
@@ -0,0 +1,13 @@
+﻿using POC.SubModule.Model1;
+
+namespace POC.SubModule.Global;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var class1 = new Class1();
+        var result = class1.Add(4, 5);
+        Console.WriteLine(result);
+    }
+}
